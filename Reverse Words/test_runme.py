d105b5c3e8d51a15acac70a3608c8b56704f1cf6
Christos Nitsas <christosn654@gmail.com>
2012-04-11 19:07:44 +0300

    Reverse Words problem

---
+++ Reverse Words/test_runme.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3.2
+"""
+Unit Tests for the Reverse Words problem 
+for Google Code Jam Africa 2010
+Qualification
+
+Link to problem description:
+http://code.google.com/codejam/contest/351101/dashboard#s=p1
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.1
+
+date:
+April, 2012
+
+usage:
+$ python3.2 test_runme.py
+or
+$ test_runme.py
+"""
+
+
+import io
+import sys
+import unittest
+# non-standard modules:
+import runme
+
+
+class TestRunme(unittest.TestCase):
+    """
+    Unit Tests for the Reverse Words problem 
+    for Google Code Jam Africa 2010
+    Qualification
+    """
+
+    # define if needed
+    #def setUp(self):
+    #    pass
+
+    # define if needed
+    #def tearDown(self):
+    #    pass
+
+    def test_main_on_sample_in(self):
+        # call runme.main and get it's output into from_main
+        with io.StringIO() as target_output_stream:
+            # redirect stdout to an io.StringIO object to run main
+            sys.stdout, old_stdout = target_output_stream, sys.stdout
+            runme.main("sample.in")
+            from_main = target_output_stream.getvalue()
+            # get original stdout back
+            sys.stdout = old_stdout
+        # get the "sample.out" file's contents
+        with open("sample.out", "r") as sample_out:
+            from_sample_out = sample_out.read()
+        # compare runme.main's results with sample.out's contents
+        self.assertEqual(from_main, from_sample_out)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
