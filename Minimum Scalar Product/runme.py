2a26d49301da16fe487d9c463399fcfc5f2d489d
Christos Nitsas <christosn654@gmail.com>
2012-04-27 06:02:59 +0300

    Minimum Scalar Product problem.

---
+++ Minimum Scalar Product/runme.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3.2
+"""
+Minimum Scalar Product problem
+for Google Code Jam 2008
+Round 1A
+
+Link to problem description:
+http://code.google.com/codejam/contest/32016/dashboard#s=p0
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.1
+
+date:
+April, 2012
+
+usage:
+$ python3.2 runme.py sample.in
+or
+$ runme.py sample.in
+(where sample.in is the input file and $ the prompt)
+"""
+
+
+import sys
+import operator
+# non-standard modules:
+from helpful import read_int, read_list_of_int
+
+
+def main(filename=None):
+    if filename is None:
+        if len(sys.argv) == 2:
+            filename = sys.argv[1]
+        else:
+            print("Usage: runme.py input_file")
+            return 1
+    with open(filename, "r") as f:
+        num_cases = read_int(f)
+        for i in range(1, num_cases + 1):
+            vector_length = read_int(f)
+            v1 = read_list_of_int(f)
+            v2 = read_list_of_int(f)
+            v1.sort()
+            v2.sort(reverse=True)
+            print("Case #{0}: {1}".format(i, sum(map(operator.mul, v1, v2))))
+    return 0
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
+
---
+++ Minimum Scalar Product/test_runme.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3.2
+"""
+Unit Tests for the Minimum Scalar Product problem 
+for Google Code Jam 2008
+Round 1A
+
+Link to problem description:
+http://code.google.com/codejam/contest/32016/dashboard#s=p0
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.1
+
+date:
+April, 2012
+
+usage:
+$ python3.2 test_runme.py
+or
+$ test_runme.py
+"""
+
+
+import io
+import sys
+import unittest
+# non-standard modules:
+import runme
+
+
+class TestRunme(unittest.TestCase):
+    """
+    Unit Tests for the Minimum Scalar Product problem 
+    for Google Code Jam 2008
+    Round 1A
+    """
+    
+    # define if needed
+    #def setUp(self):
+    #    pass
+    
+    # define if needed
+    #def tearDown(self):
+    #    pass
+    
+    #def test_something(self):
+    #    # use self.assertEqual(), self.assertTrue() or self.assertRaises()
+    #    pass
+    
+    def test_main_on_sample_in(self):
+        # call runme.main and get it's output into from_main
+        with io.StringIO() as target_output_stream:
+            # redirect stdout to an io.StringIO object to run main
+            sys.stdout, old_stdout = target_output_stream, sys.stdout
+            runme.main("sample.in")
+            from_main = target_output_stream.getvalue()
+            # get original stdout back
+            sys.stdout = old_stdout
+        # get the "sample.out" file's contents
+        with open("sample.out", "r") as sample_out:
+            from_sample_out = sample_out.read()
+        # compare runme.main's results with sample.out's contents
+        self.assertEqual(from_main, from_sample_out)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
