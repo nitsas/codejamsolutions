ffa8641154b0ed128e01f7d2e618b0dc4afacf96
Christos Nitsas <christosn654@gmail.com>
2012-05-05 18:55:34 +0300

    FreeCell Statistics problem.

---
+++ FreeCell Statistics/runme.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3.2
+"""
+Freecell Statistics problem
+for Google Code Jam 2011
+Round 1A
+
+Link to problem description:
+http://code.google.com/codejam/contest/1145485/dashboard#s=p0
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.1
+
+date:
+April, 2012
+
+usage:
+$ python3.2 runme.py sample.in
+or
+$ runme.py sample.in
+(where sample.in is the input file and $ the prompt)
+"""
+
+
+import sys
+# non-standard modules:
+from helpful import read_int, read_list_of_int
+
+
+def is_it_possible(N, PD, PG):
+    if PG == 0:
+        if PD == 0:
+            return "Possible"
+        else:
+            return "Broken"
+    elif PG == 100:
+        if PD == 100:
+            return "Possible"
+        else:
+            return "Broken"
+    else:
+        for d in range(1, N + 1):
+            if (PD * d) % 100.0 == 0.0:
+                return "Possible"
+    return "Broken"
+
+
+def main(filename=None):
+    if filename is None:
+        if len(sys.argv) == 2:
+            filename = sys.argv[1]
+        else:
+            print("Usage: runme.py input_file")
+            return 1
+    with open(filename, "r") as f:
+        num_test_cases = read_int(f)
+        for i in range(1, num_test_cases + 1):
+            N, PD, PG = read_list_of_int(f)
+            print("Case #{0}: {1}".format(i, is_it_possible(N, PD, PG)))
+    return 0
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
+
