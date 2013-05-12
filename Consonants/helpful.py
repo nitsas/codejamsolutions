07fa70035adbcbfc074cc7e4310749f640c63ebb
Christos Nitsas <christosn654@gmail.com>
2013-05-12 16:52:47 +0300

    Added the Consonants problem.

---
+++ Consonants/helpful.py
@@ -0,0 +1,38 @@
+"""
+A collection of small, helpful functions I'll be using a lot.
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.3
+
+date:
+April, 2012
+"""
+
+
+__name__ = "helpful"
+__all__  = ["read_int", "read_float", "read_list_of_str", 
+            "read_list_of_int", "read_list_of_float"]
+
+
+# In every function below, 'file' is an open file object.
+
+
+def read_int(file):
+    return int(file.readline())
+
+def read_float(file):
+    return float(file.readline())
+
+def read_list_of_str(file):
+    return file.readline().split()
+
+def read_list_of_int(file):
+    return list(map(int, read_list_of_str(file)))
+
+def read_list_of_float(file):
+    return list(map(float, read_list_of_str(file)))
+
---
+++ Consonants/runme.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3.3
+"""
+Consonants problem
+for Google Code Jam 2013
+Round 1C
+
+Link to problem description:
+https://code.google.com/codejam/contest/2437488/dashboard#s=p0
+
+author: 
+Christos Nitsas
+(chrisn654)
+
+language:
+Python 3.2.3
+
+date:
+May, 2012
+
+usage:
+$ python3.2 runme.py sample.in
+or
+$ runme.py sample.in
+(where sample.in is the input file and $ the prompt)
+"""
+
+
+import sys
+import re
+# non-standard modules:
+from helpful import read_int, read_list_of_str
+
+
+def match_positions(string, n):
+    num_consecutive_consonants = 0
+    for i in range(len(string)):
+        if string[i] in 'aeiou':
+            # encountered a vowel
+            num_consecutive_consonants = 0
+        else:
+            # encountered a consonant
+            num_consecutive_consonants += 1
+            if num_consecutive_consonants >= n:
+                # found another match
+                yield i - n + 1
+
+
+def compute_n_value(name, n):
+    n_value = 0
+    leftmost_position = 0
+    for mi in match_positions(name, n):
+        # We want to count the number of substrings that start on 
+        # leftmost_position or later and contain match mi:
+        # - there are (mi - leftmost_position + 1) different options for the 
+        #   start position of the substring 
+        # - and (len(name) - mi - n + 1) different options for its end position
+        n_value += (mi - leftmost_position + 1) * (len(name) - mi - n + 1)
+        leftmost_position = mi + 1
+    return n_value
+
+
+def main(filename=None):
+    if filename is None:
+        if len(sys.argv) == 2:
+            filename = sys.argv[1]
+        else:
+            print("Usage: runme.py input_file")
+            return 1
+    with open(filename, "r") as f:
+        num_test_cases = read_int(f)
+        for i in range(1, num_test_cases + 1):
+            name, num = read_list_of_str(f)
+            n = int(num)
+            print("Case #{0}:".format(i), end=" ")
+            print(compute_n_value(name, n))
+    return 0
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
+
