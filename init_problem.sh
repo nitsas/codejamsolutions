6a7896287ea94657c21ed2c4fd74f05ae4a9cbb7
nitsas <nitsas.chris@gmail.com>
2014-11-27 21:49:19 +0200

    [init_problem] Update problem initialization file

--- init_problem.sh
+++ init_problem.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Quickly initialize a problem directory with templates and helpful files.
 
 
+# did we get all 4 input parameters we expected?
 if [ $# -ne 4 ]
 then
   echo 'Usage: init_problem.sh "Problem Name" Year "Round" Link'
@@ -8,20 +10,25 @@ then
 fi
 
 
+# read input parameters into vars
 name=${1:-DefaultProblemName}
 year=${2:-DefaultYear}
 round=${3:-DefaultRound}
 link=${4:-DefaultLink}
+date=${5:-`date +"%B, %Y"`}
 
 
+# make problem directory
 mkdir "$name"
-cp runme-template.py "$name/runme.py"
-cp test_runme-template.py "$name/test_runme.py"
+# copy templates and helpful files
+cp runme_template.py "$name/runme.py"
+cp test_runme_template.py "$name/test_runme.py"
 cp helpful.py "$name/helpful.py"
 
 
+# fill-in problem info inside the templates
 sed -i "" "s/--Problem Name--/$name/g" "$name/runme.py" "$name/test_runme.py"
 sed -i "" "s/--Year--/$year/g" "$name/runme.py" "$name/test_runme.py"
 sed -i "" "s/--Round--/$round/g" "$name/runme.py" "$name/test_runme.py"
 sed -i "" "s,--Link--,$link,g" "$name/runme.py" "$name/test_runme.py"
-
+sed -i "" "s/--Date--/$date/g" "$name/runme.py" "$name/test_runme.py"
