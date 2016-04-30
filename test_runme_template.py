d5764ab6521054436a817893164a68f7b9e8fbd8
Chris Nitsas <nitsas.chris@gmail.com>
2016-04-30 22:09:46 +0300

    [test_runme_template] Add support for extra sample input and output

--- test_runme_template.py
+++ test_runme_template.py
@@ -23,10 +23,32 @@ Usage:
 
 
 import io
+import os
 import sys
 import unittest
 # modules I've written:
 import runme
+
+
+def are_extra_samples_present():
+    return os.path.isfile('extra_sample.in') and os.path.isfile('extra_sample.out')
+
+
+def contents_of(output_file):
+    with open(output_file, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def output_of_runme_on(input_file):
+    # call runme.main and get its output into from_main
+    with io.StringIO() as target_output_stream:
+        # redirect stdout to an io.StringIO object to run main
+        sys.stdout, old_stdout = target_output_stream, sys.stdout
+        runme.main(input_file)
+        from_main = target_output_stream.getvalue()
+        # get original stdout back
+        sys.stdout = old_stdout
+    return from_main
 
 
 class TestRunme(unittest.TestCase):
@@ -48,19 +70,17 @@ class TestRunme(unittest.TestCase):
     #     pass
     #
     def test_main_on_sample_in(self):
-        # call runme.main and get its output into from_main
-        with io.StringIO() as target_output_stream:
-            # redirect stdout to an io.StringIO object to run main
-            sys.stdout, old_stdout = target_output_stream, sys.stdout
-            runme.main('sample.in')
-            from_main = target_output_stream.getvalue()
-            # get original stdout back
-            sys.stdout = old_stdout
-        # get the 'sample.out' file's contents
-        with open('sample.out', 'r', encoding='utf-8') as sample_out:
-            from_sample_out = sample_out.read()
+        input_file, output_file = 'sample.in', 'sample.out'
         # compare runme.main's results with sample.out's contents
-        self.assertEqual(from_main, from_sample_out)
+        self.assertEqual(output_of_runme_on(input_file),
+                         contents_of(output_file))
+    
+    @unittest.skipIf(not are_extra_samples_present(), 'no extra samples')
+    def test_main_on_extra_sample_in(self):
+        input_file, output_file = 'extra_sample.in', 'extra_sample.out'
+        # compare runme.main's results with extra_sample.out's contents
+        self.assertEqual(output_of_runme_on(input_file),
+                         contents_of(output_file))
 
 
 if __name__ == '__main__':
